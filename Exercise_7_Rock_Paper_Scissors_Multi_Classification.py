7e354d6d741f749bece2ae40aa6a1de5a42ffbf2
Vamsidhar Talasila <talasila.chinna@gmail.com>
2024-07-05 23:24:29 +0530

    Update Exercise_7_Rock_Paper_Scissors_Multi_Classification.py

--- Exercise_7_Rock_Paper_Scissors_Multi_Classification.py
+++ Exercise_7_Rock_Paper_Scissors_Multi_Classification.py
@@ -63,36 +63,3 @@ model = tf.keras.models.Sequential([
 # Print the model summary
 model.summary()
 
-model.compile(loss = 'categorical_crossentropy', optimizer='rmsprop', metrics=['accuracy'])
-
-TRAINING_DIR = "tmp/rps-train/rps"
-training_datagen = ImageDataGenerator(
-      rescale = 1./255,
-	  rotation_range=40,
-      width_shift_range=0.2,
-      height_shift_range=0.2,
-      shear_range=0.2,
-      zoom_range=0.2,
-      horizontal_flip=True,
-      fill_mode='nearest')
-
-VALIDATION_DIR = "tmp/rps-test/rps-test-set"
-validation_datagen = ImageDataGenerator(rescale = 1./255)
-
-train_generator = training_datagen.flow_from_directory(
-	TRAINING_DIR,
-	target_size=(150,150),
-	class_mode='categorical',
-    batch_size=126
-)
-
-validation_generator = validation_datagen.flow_from_directory(
-	VALIDATION_DIR,
-	target_size=(150,150),
-	class_mode='categorical',
-    batch_size=126
-)
-
-history = model.fit(train_generator, epochs=25, steps_per_epoch=20, validation_data = validation_generator, verbose = 1, validation_steps=3)
-
-
